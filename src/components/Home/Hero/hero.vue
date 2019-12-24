27c19c6e737a292d53c22b1f80bf552390ee142b
aloksharmaas <alok.sharma61630@gmail.com>
2019-12-24 05:01:21 -0800

    Documentation Changed

---
+++ src/components/Home/Hero/hero.vue
@@ -0,0 +1,88 @@
+<template>
+ 
+    <section id="Hero">
+
+        <home-header></home-header>
+        
+        <div class="herobox">
+            <h2>Welcome to the <span>Ultimate</span> Buisness Directory</h2>
+            <p>
+                This is the <span>premium Destination</span> for decison makers.<br>
+                Find a product,service,or company you're looking for.
+            </p>
+        </div>
+
+        <home-search-box></home-search-box>
+        <home-gradient></home-gradient>
+     
+    </section>
+
+</template>
+
+
+<script>
+
+import Header from "./header";
+import SearchBox from "./searchBox";
+import Gradient from "./gradient";
+
+
+export default {
+    components:{
+        "home-header":Header,
+        "home-search-box":SearchBox,
+        "home-gradient":Gradient    
+    }
+  
+}
+
+</script>
+
+
+<style scoped>
+#Hero{
+    width: 100%;
+    height: 130vh;
+    background: url("../../../assets/background2.jpg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-attachment: fixed;
+    background-size: 100% 100%;
+    position: relative;
+    z-index: 10;
+}
+
+/* .overlay{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0,0,0,0.01);
+    z-index: 20;
+
+} */
+
+.herobox{
+    color: white;
+    z-index: 30;
+    position: absolute;
+    top: 28%;
+    left: 8%;
+}
+
+.herobox h2 {
+    font-size:4.2rem;
+    font-weight: 500;
+    margin-bottom: 1rem;
+}
+
+.herobox span{
+    font-weight: 700;
+}
+
+.herobox p {
+    font-size: 2rem;
+}
+
+</style>
--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,34 +1,28 @@
 <template>
-  <div id="home">
-      <!-- <div class="overlay"></div> -->
-      <home-header></home-header>
+  
+  <div id="Home">
       
-      <div class="herobox">
-        <h2>Welcome to the <span>Ultimate</span> Buisness Directory</h2>
-        <p>
-            This is the <span>premium Destination</span> for decison makers.<br>
-            Find a product,service,or company you're looking for.
-        </p>
-      </div>
+    <!-- Hero Section -->
+    <home-hero></home-hero>
+    
+    <!-- Featured Section -->
+    <home-featured></home-featured>
 
-      <home-search-box></home-search-box>
-      <home-gradient></home-gradient>
-      
   </div>
+
+
 </template>
 
 
 <script>
 
-import Header from "./HomeComponents/header";
-import SearchBox from "./HomeComponents/searchBox";
-import Gradient from "./HomeComponents/gradient";
+import Hero from "./Hero/hero.vue";
+import Featured from "./Featured/featured.vue"
 
 export default {
     components:{
-        "home-header":Header,
-        "home-search-box":SearchBox,
-        "home-gradient":Gradient
+        "home-hero":Hero,
+        "home-featured":Featured
     }
   
 }
@@ -36,49 +30,10 @@ export default {
 
 
 <style scoped>
-#home{
-    width: 100%;
-    height: 130vh;
-    background: url("../../assets/background2.jpg");
-    background-repeat: no-repeat;
-    background-position: center;
-    background-attachment: fixed;
-    background-size: 100% 100%;
-    position: relative;
-    z-index: 10;
-}
 
-/* .overlay{
-    position: absolute;
-    top: 0;
-    left: 0;
+#Home{
     width: 100%;
     height: 100%;
-    background-color: rgba(0,0,0,0.01);
-    z-index: 20;
-
-} */
-
-.herobox{
-    color: white;
-    z-index: 30;
-    position: absolute;
-    top: 28%;
-    left: 8%;
-}
-
-.herobox h2 {
-    font-size:4.2rem;
-    font-weight: 500;
-    margin-bottom: 1rem;
-}
-
-.herobox span{
-    font-weight: 700;
-}
-
-.herobox p {
-    font-size: 2rem;
 }
   
 </style>
